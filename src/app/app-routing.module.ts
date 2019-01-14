3a24796d575279dc9b9e0dced9bf35bbf7bfa778
zhongjingjogy <zhongjingjogy@gmail.com>
2019-01-14 20:55:46 +0800

    update: use hash in the URL.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,6 +17,9 @@ const routes: Routes = [
 
 ];
 
-@NgModule({imports: [RouterModule.forRoot(routes)], exports: [RouterModule]})
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  exports: [RouterModule]
+})
 export class AppRoutingModule {
 }
