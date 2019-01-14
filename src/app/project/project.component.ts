89a01df059aa97c468041b7da494b9dd0b32df87
zhongjingjogy <zhongjingjogy@gmail.com>
2019-01-14 20:56:15 +0800

    update: call for login when project information is not ready.

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { HitdicserviceService } from '../hitdicservice.service';
-import { Project } from '../project';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
+
+import {HitdicserviceService} from '../hitdicservice.service';
+import {Project} from '../project';
 
 @Component({
   selector: 'app-project',
@@ -9,23 +10,28 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./project.component.css']
 })
 export class ProjectComponent implements OnInit {
-
   projects: Project[];
-  constructor(private hitdicservice: HitdicserviceService, private route: ActivatedRoute,
-    private router: Router) {
-    
-   }
+  constructor(
+      private hitdicservice: HitdicserviceService,
+      private route: ActivatedRoute, private router: Router) {
+    // console.log("in project constructor");
+  }
 
   ngOnInit() {
+    // console.log("in project OnInit");
     if (!this.hitdicservice.status) {
       this.router.navigate(['/login']);
     }
+
     this.projects = this.hitdicservice.projects;
-    console.log(this.projects);
+    if (!this.projects) {
+      console.log(this.projects);
+      console.log(
+          'Fail to retreive the project information, please login again!');
+    }
   }
 
   listTasks(hid: string) {
     this.router.navigate(['/project/' + hid]);
   }
-
 }
